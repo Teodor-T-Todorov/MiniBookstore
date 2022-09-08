0b61963913644a4c1d273e555dfa7031d55aaf66
Teodor <teodor.t.todorov@outlook.com>
2022-09-08 19:08:13 +0300

    Add functionality and log messages to Product and Bookstore classes

--- src/main/java/bookstore/Bookstore.java
+++ src/main/java/bookstore/Bookstore.java
@@ -1,4 +1,84 @@
 package bookstore;
 
+import bookstore.products.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 public class Bookstore {
+    private String name;
+    private List<Product> productList;
+    private final Logger log = LoggerFactory.getLogger(Bookstore.class);
+
+    public Bookstore(String name, List<Product> productList) {
+        this.name = name;
+        this.productList = productList;
+    }
+
+    public void addProductToStore(Product product) {
+        if (!productList.contains(product)) {
+            productList.add(product);
+            log.info("{} copies of [{}, {}] got added to the bookstore",
+                    product.getCopies(),
+                    product.getName(),
+                    product.getClass().getSimpleName());
+        } else {
+            log.info("[{}, {}] is already available in the store",
+                    product.getName(), product.getClass().getSimpleName());
+        }
+    }
+
+    public void removeProductFromStore(Product product) {
+        if (productList.contains(product)) {
+            productList.remove(product);
+            log.info("[{}, {}] got removed to the bookstore",
+                    product.getName(), product.getClass().getSimpleName());
+        } else {
+            log.info("Can't remove, [{}, {}] is not included in the store",
+                    product.getName(), product.getClass().getSimpleName());
+        }
+    }
+
+    public void sellProduct(Product product) {
+        if (productList.contains(product)) {
+            product.sellCopies(1);
+        } else {
+            log.info("[{}, {}] is not currently in stock",
+                    product.getName(), product.getClass().getSimpleName());
+        }
+    }
+
+    public void sellProduct(Product product, int copies) {
+        if (productList.contains(product)) {
+            product.sellCopies(copies);
+        } else {
+            log.info("[{}, {}] is not currently in stock",
+                    product.getName(), product.getClass().getSimpleName());
+        }
+    }
+
+    public void resupplyProduct(Product product, int copies) {
+        if (productList.contains(product)) {
+            product.orderCopies(copies);
+        } else {
+            log.info("[{}, {}] is not currently in stock",
+                    product.getName(), product.getClass().getSimpleName());
+        }
+    }
+
+    public void makeDiscount(Product product, double discount) {
+        if (discount > 0 && discount < 100) {
+            if (productList.contains(product)) {
+                product.setPrice(product.getPrice() - product.getPrice()*(discount/100));
+                log.info("Discount price for [{}, {}] is {}$",
+                        product.getName(), product.getClass().getSimpleName(), product.getPrice());
+            } else {
+                log.info("[{}, {}] is not currently in stock",
+                        product.getName(), product.getClass().getSimpleName());
+            }
+        } else {
+            log.warn("Invalid discount number");
+        }
+    }
 }
--- src/main/java/bookstore/products/Product.java
+++ src/main/java/bookstore/products/Product.java
@@ -1,4 +1,61 @@
 package bookstore.products;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 abstract public class Product {
+    private String name;
+    private double price;
+    private int copies;
+    private final Logger log = LoggerFactory.getLogger(Product.class);
+
+    public Product(String name, double price, int copies) {
+        this.name = name;
+        this.price = price;
+        this.copies = copies;
+    }
+
+    abstract public String getProductInfo();
+
+    public void sellCopies(int copiesToSell) {
+        if (copies >= copiesToSell) {
+            copies -= copiesToSell;
+            log.info("Sold {} copies of {}", copiesToSell, name);
+        } else {
+            log.info("""
+                    Not enough copies in stock.\s
+                    Current copies {}\s
+                    Copies wanted to be sold {}""", copies,copiesToSell);
+        }
+    }
+
+    public void orderCopies(int copiesToOrder) {
+        copies += copiesToOrder;
+        log.info("{} copies has been added for a total of {} for product: {}",
+                copiesToOrder, copies, getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCopies() {
+        return copies;
+    }
+
+    public void setCopies(int copies) {
+        this.copies = copies;
+    }
 }
